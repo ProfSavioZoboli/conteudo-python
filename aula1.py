2ca1afa33949a4b729ad05eac020a30ed7433d9b
SAVIO EDUARDO ZOBOLI <savio_zoboli@estudante.sc.senai.br>
2025-10-03 09:48:38 -0300

    aula 1: comando print

---
+++ aula1.py
@@ -0,0 +1,14 @@
+# Isso é um comentário
+# Vamos escapar da maldição
+
+#Esse é o comando print,
+#ele serve para imprimir na tela
+print("Olá mundo!")
+
+#Podemos mudar o comportamento da finalização
+#do comando com o parâmetro end
+print("Esse é o curso de:",end=" ")
+print("Lógica","de","programação")
+#Podemos mudar o comportamento do separador
+#com o parâmetro sep
+print("Frase","com","hifen",sep="-")
